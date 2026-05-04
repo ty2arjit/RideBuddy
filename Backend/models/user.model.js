e6266eaf01258a73dce83051c48b700c4cc97c55
ty2arjit <ty2arjit@gmail.com>
2026-05-04 11:39:55 +0530

    feat : user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,51 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Token = process.env.JWT_SECRET;
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [2, 'First name must have at least 2 characters'],
+    },
+    lastname: {
+      type: String,
+    },
+  },
+  email : {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Password must have at least 6 characters'],
+    select: false,
+  },
+  socketId: {
+    type: String,
+  }
+})
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, Token, { expiresIn: '24h' });
+  return token;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.hashPassword = async function( password ) {
+  return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
