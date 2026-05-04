a5f3c21e8f1625e182721388d631e69f750d9f50
ty2arjit <ty2arjit@gmail.com>
2026-05-04 11:39:37 +0530

    feat : database configured

---
+++ Backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+// const MongoURI = process.env.MONGO_URI;
+
+
+const connectDB = async() => {
+  try {
+    await mongoose.connect(MongoURI);
+    console.log('Connected to MongoDB successfully');
+  } catch(error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
