3653629820e93298eb958a94366270b33b42b093
ty2arjit <ty2arjit@gmail.com>
2026-05-08 10:59:14 +0530

    feat : added the middleware for captain authentication

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const userModel = require('../models/user.model');
+const captainModel = require('../models/captain.model');
 const jwt = require('jsonwebtoken');
 const JWT_KEY = process.env.JWT_SECRET;
-const BlacklistToken = require('../models/blacklistToken.model');
+const BlacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next ) => {
   const token = req.cookies?.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
@@ -12,7 +13,7 @@ module.exports.authUser = async (req, res, next ) => {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  const isBlacklisted = await BlacklistToken.findOne({ token: token });
+  const isBlacklisted = await BlacklistTokenModel.findOne({ token: token });
 
   if( isBlacklisted ) {
     return res.status(401).json({ message: 'Unauthorized' });
@@ -34,3 +35,31 @@ module.exports.authUser = async (req, res, next ) => {
   }
 }
 
+module.exports.authCaptain = async(req, res, next) => {
+  const token = req.cookies?.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+
+  if( !token ) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const isBlacklisted = await BlacklistTokenModel.findOne({ token: token });
+
+  if( isBlacklisted ) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_KEY);
+    const captain = await captainModel.findById(decoded._id);
+
+    if( !captain ) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    } 
+
+    req.captain = captain;
+
+    return next();
+  } catch(error) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+}
