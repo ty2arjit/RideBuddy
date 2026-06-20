3e42a24fe1ada6f04b28a1a74634a9e9e803d806
ty2arjit <ty2arjit@gmail.com>
2026-06-20 18:07:24 +0530

    fix: updated auth middleware

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,65 +1,69 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const userModel = require('../models/user.model');
-const captainModel = require('../models/captain.model');
-const jwt = require('jsonwebtoken');
+const userModel = require("../models/user.model");
+const captainModel = require("../models/captain.model");
+const jwt = require("jsonwebtoken");
 const JWT_KEY = process.env.JWT_SECRET;
-const BlacklistTokenModel = require('../models/blacklistToken.model');
+const BlacklistTokenModel = require("../models/blacklistToken.model");
 
-module.exports.authUser = async (req, res, next ) => {
-  const token = req.cookies?.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+module.exports.authUser = async (req, res, next) => {
+  const token =
+    req.cookies?.token ||
+    (req.headers.authorization && req.headers.authorization.split(" ")[1]);
 
-  if( !token ) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   const isBlacklisted = await BlacklistTokenModel.findOne({ token: token });
 
-  if( isBlacklisted ) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     const decoded = jwt.verify(token, JWT_KEY);
     const user = await userModel.findById(decoded._id);
 
-    if( !user ) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    } 
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
     req.user = user;
 
     return next();
-  } catch(error) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
-}
+};
 
-module.exports.authCaptain = async(req, res, next) => {
-  const token = req.cookies?.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+module.exports.authCaptain = async (req, res, next) => {
+  const token =
+    req.cookies?.token ||
+    (req.headers.authorization && req.headers.authorization.split(" ")[1]);
 
-  if( !token ) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   const isBlacklisted = await BlacklistTokenModel.findOne({ token: token });
 
-  if( isBlacklisted ) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     const decoded = jwt.verify(token, JWT_KEY);
     const captain = await captainModel.findById(decoded._id);
 
-    if( !captain ) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    } 
+    if (!captain) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
     req.captain = captain;
 
     return next();
-  } catch(error) {
-    return res.status(401).json({ message: 'Unauthorized' });
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
-}
+};
